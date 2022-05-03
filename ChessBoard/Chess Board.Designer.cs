5582cc61c9dc209e11f208423ba6648b320be38e
Melih Özaslan <melihozaslan01@gmail.com>
2022-05-03 20:05:13 +0300

    Tap to Create Chess Board

---
+++ ChessBoard/Chess Board.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace ChessBoard
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CrtChssBoard = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // CrtChssBoard
+            // 
+            this.CrtChssBoard.Location = new System.Drawing.Point(574, 1);
+            this.CrtChssBoard.Name = "CrtChssBoard";
+            this.CrtChssBoard.Size = new System.Drawing.Size(169, 562);
+            this.CrtChssBoard.TabIndex = 0;
+            this.CrtChssBoard.Text = "Create Cheess Board";
+            this.CrtChssBoard.UseVisualStyleBackColor = true;
+            this.CrtChssBoard.Click += new System.EventHandler(this.CrtChssBoard_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 563);
+            this.Controls.Add(this.CrtChssBoard);
+            this.Name = "Form1";
+            this.Text = "Chess Board";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CrtChssBoard;
+    }
+}
+
---
+++ ChessBoard/Chess Board.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChessBoard
+{
+    public partial class Form1 : Form
+    {
+        
+        int top = 0;
+        int left = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CrtChssBoard_Click(object sender, EventArgs e)
+        {
+            AddPnlBtnClick();
+        }
+        private void AddPnlBtnClick ()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    Panel[,] panel = new Panel[8, 8];
+                    panel[i, j] = new Panel();
+                    panel[i, j].Width = 70;
+                    panel[i, j].Height = 70;
+                    panel[i, j].Left = left;
+                    panel[i, j].Top = top;
+                    this.Controls.Add(panel[i, j]);
+                    left += 70;
+                    if ((i + j) % 2 == 0)
+                    {
+                        panel[i, j].BackColor = Color.Black;
+                    }
+                    else
+                    {
+                        panel[i, j].BackColor = Color.White;
+                    }
+                }
+                top += 70;
+                left = 0;
+            }
+        }
+    }
+}
+
